199bd887de4c5993ed79c4038ff880fbbe0c527f
msheehan7 <52415615+msheehan7@users.noreply.github.com>
2020-06-02 19:54:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var d1 = document.getElementById('distance1').value*39.3701;
 var d2 = document.getElementById('distance2').value*39.3701;
 var d3 = document.getElementById('distance3').value*39.3701;
 var initialCalc = ((m1*d1)+(m2*d2)+(m3*d3))/m4
-var convertedForm = Math.floor(initialCalc*-.0254)
+var convertedForm = (initialCalc*-.0254).toFixed(2);
     document.getElementById("h1").innerHTML = convertedForm+" inches from center";
     event.preventDefault();
   });
